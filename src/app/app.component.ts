c9d5d4b58bcd0faab9c356602a9f4546beced614
mehdiben01 <mehdibehmidi51@gmail.com>
2024-04-15 15:19:54 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'totaldemo-app';
+  actions : Array<any> = [
+    {
+      title: "home", route : "/home", icon :"house"
+    },
+    {
+      title: "Products", route : "/products", icon :"search"
+    },
+    {
+      title: "New Product", route : "/new-product", icon :"save"
+    },
+
+  ];
+  currentAction : any;
+
+  setCurrentAction(action: any) {
+    this.currentAction = action;
+
+  }
 }
